bd40086f5744d5bb38de05d6ba03ac22451f202d
Gino Garcia <gino_garcia@hotmail.com>
2019-02-23 20:45:07 -0400

    experimenting cmake

---
+++ build/CMakeFiles/compute-areas.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/giarinogarcia/Downloads/example/compute-areas.cpp" "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/compute-areas.dir/compute-areas.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/geometry.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/geometry.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/giarinogarcia/Downloads/example/geometry_circle.cpp" "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/geometry.dir/geometry_circle.cpp.o"
+  "/Users/giarinogarcia/Downloads/example/geometry_polygon.cpp" "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/geometry.dir/geometry_polygon.cpp.o"
+  "/Users/giarinogarcia/Downloads/example/geometry_rhombus.cpp" "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/geometry.dir/geometry_rhombus.cpp.o"
+  "/Users/giarinogarcia/Downloads/example/geometry_square.cpp" "/Users/giarinogarcia/Downloads/example/build/CMakeFiles/geometry.dir/geometry_square.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
